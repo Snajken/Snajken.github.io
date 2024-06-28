b5f0f008b5f62bdf1575c9e23ab7b509f3963086
Snajken <118436466+Snajken@users.noreply.github.com>
2024-06-28 16:18:33 +0200

    Create getMail.js

---
+++ Diverse saker/getMail.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+(function () {
+
+
+
+
+    
+    Office.onReady(function () {
+
+        $(document).ready(function () {
+            // The document is ready
+            loadItemProps(Office.context.mailbox.item);
+        });
+    });
+        
+    function loadItemProps(item) {
+        // Write message property values to the task pane
+        $('#item-id').text(item.itemId);
+        $('#item-subject').text(item.subject);
+        $('#item-internetMessageId').text(item.internetMessageId);
+        $('#item-from').html(item.from.displayName + " &lt;" + item.from.emailAddress + "&gt;");
+    }
+})();
